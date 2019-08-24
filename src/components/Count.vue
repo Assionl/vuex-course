872f079012f215a33b8793dac1f89087f55c5673
Assionl <740809230@qq.com>
2019-08-24 15:24:04 +0800

    lesson-1

---
+++ src/components/Count.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="count">
+    <div>
+      <hr />
+      <h1>count.vue:{{count}}</h1>
+      <p>count.vue:{{todos}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  name: "count",
+  components: {},
+  //   computed:{
+  //       ...mapState:(["count","todos"])
+  //   }
+  computed: {
+    count() {
+      return this.$store.state.count;
+    },
+    todos() {
+      return this.$store.state.todos;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
